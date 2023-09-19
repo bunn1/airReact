8c8ae310af0456b8e145b6c563f63e5d5893df55
bunn1 <nicjohbun@gmail.com>
2023-09-19 12:27:45 +0200

    cleaning and inserting logo

--- airreact/src/App.js
+++ airreact/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/airbnb-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <img src={logo} className="logo-img" alt="logo" width="50px"/>
+      <h2 className="head-title">airbnb</h2>
+    </nav>
+
   );
 }
 
