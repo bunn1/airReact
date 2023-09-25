aab8e269bb15ccbdc7f065f2b3cde96bbe067dfd
bunn1 <nicjohbun@gmail.com>
2023-09-25 16:08:11 +0200

    all

--- airreact/src/component/Contact.jsx
+++ airreact/src/component/Contact.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-// import cat from '../images/cat.jpg';
-// import phone from '../images/phone-icon.png';
-// import mail from '../images/mail-icon.png';
 
 function Contact (props) {
 
-  const {img, name, phone, mail} = props;
+  // const {img, name, phone, mail} = props;
   
     return (
     
     
         <div className="contact-card">
-        <img src={img} alt="cat" />
-        <h3>{name}</h3>
+        <img src={props.img} alt="cat" />
+        <h3>{props.name}</h3>
         <div className="info-group">
-          <img className="phone-icon" src={phone} alt="phone" />
-          <p>{phone}</p>
+          <img className="phone-icon" src={props.phoneImg} alt="phone" width="15px" height="15px"/>
+          <p>{props.phone}</p>
         </div>
         <div className="info-group">
-          <img className="mail-icon" src={mail} alt="mail" />
-          <p>{mail}</p>
+          <img className="mail-icon" src={props.mailIcons} alt="mail" />
+          <p>{props.mail}</p>
+
         </div>
       </div>
     )
 
 }
-
 export default Contact;
--- airreact/src/index.js
+++ airreact/src/index.js
@@ -14,26 +14,34 @@ root.render(
     <Hero />
     <Card />
     <Contact 
-      img="../images/litenkatt.jpg"
+      img="/images/litenkatt.jpg"
       name="Mr Whiskersson"
+      phoneImg="/images/phone-icon.png"
       phone="555-555-5555"
+      mailIcons="/images/mail-icon.png"
       mail="whiskas@catnap.meow"
     /> <Contact 
-    img="../images/litenkatt.jpg"
+    img="/images/litenkatt.jpg"
     name="Mr Nilsson"
+    phoneImg="/images/phone-icon.png"
     phone="666-666-6666"
+    mailIcons="/images/mail-icon.png"
     mail="junk@gmail.com"
   />
  <Contact 
-      img="../images/litenkatt.jpg"
+      img="/images/litenkatt.jpg"
       name="Mr Andersson"
+      phoneImg="/images/phone-icon.png"
       phone="777-777-777"
+      mailIcons="/images/mail-icon.png"
       mail="hoppsan@gmail.com"
     />
  <Contact 
-      img="../images/litenkatt.jpg"
+      img="/images/litenkatt.jpg"
       name="Mr Pettersson"
+      phoneImg="/images/phone-icon.png"
       phone="888-888-8888"
+      mailIcons="/images/mail-icon.png"
       mail="hoho@gmail.com"
     />
 
